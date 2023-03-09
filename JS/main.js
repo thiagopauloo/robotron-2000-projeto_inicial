539889fd1a044d256c54bb5a185aa5bd8b6473e2
thiagopauloo <thiaggopaulo@gmail.com>
2023-03-09 12:13:03 -0300

    atualizando meu projeto robotron

--- JS/main.js
+++ JS/main.js
@@ -36,24 +36,24 @@ const pecas = {
 }
 
 controle.forEach( (elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
         atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
-    
+
     if(operacao === "-") {
-        peca.value = parseInt(peca.value) - 1
+        peca.value = parseInd(peca.value) - 1
     } else {
         peca.value = parseInt(peca.value) + 1
     }
 }
 
 function atualizaEstatistica(peca) {
-    estatistica.forEach( (elemento) => {
+    estatistica.forEach( (elemento ) => {
         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
     })
 }
